98955027bd07246b4348cf027ae3da415adc171e
Ricll <ricllima@hotmail.com>
2020-12-09 18:54:22 -0300

    Add Opacity Transiction Portifolio Text

--- src/components/Welcome/index.tsx
+++ src/components/Welcome/index.tsx
@@ -1,54 +1,57 @@
 import React from 'react'
-import { useViewportScroll, useTransform } from 'framer-motion'
+import {
+  useViewportScroll,
+  useTransform,
+  AnimateSharedLayout
+} from 'framer-motion'
 import {
   Container,
   Section,
+  ProfileImg,
   WelcomeTitle,
   WelcomeTitleContainer,
   PortifolioText,
   PortifolioContainer
 } from './styles'
 
-import logo from '../../assets/reactandnative.jpeg'
-import profile from '../../assets/perfil.jpg'
+import logo from '../../assets/react-logo.png'
 
 const Welcome: React.FC = () => {
   const { scrollYProgress } = useViewportScroll()
-
   const ContainerPositiondScale = useTransform(
     scrollYProgress,
-    [0.0, 0.172, 0.187, 0.2, 0.25],
+    [0.0, 0.022, 0.33, 0.05, 0.09],
     ['fixed', 'fixed', 'fixed', 'fixed', 'absolute']
   )
 
   const SectionWidhtScale = useTransform(
     scrollYProgress,
-    [0.0, 0.112, 0.115],
-    ['47vw', '47vw', '94vw']
+    [0.0, 0.002, 0.013],
+    ['50%', '100%', '100%']
   )
 
   const WelcomeMarginLeftScale = useTransform(
     scrollYProgress,
-    [0.0, 0.112],
+    [0.0, 0.002],
     ['100%', '200%']
   )
 
   const WelcomeTextTransitionOpacity = useTransform(
     scrollYProgress,
-    [0.0, 0.112],
+    [0.0, 0.002],
     ['0ms', '1500ms']
   )
 
   const WelcomeTextComeOpacity = useTransform(
     scrollYProgress,
-    [0.0, 0.117, 0.126],
-    ['1', '0.9', '0']
+    [0.0, 0.002, 0.013],
+    ['1', '0', '0']
   )
 
-  const ProtifolioOpacity = useTransform(
+  const PortifolioOpacity = useTransform(
     scrollYProgress,
-    [0.0, 0.117, 0.126],
-    ['0', '0.9', '1']
+    [0.0, 0.002, 0.013],
+    ['0', '1', '1']
   )
   return (
     <Container style={{ position: ContainerPositiondScale }}>
@@ -57,14 +60,18 @@ const Welcome: React.FC = () => {
         transition={{ ease: 'easeOut' }}
       >
         <PortifolioContainer
-          style={{ opacity: ProtifolioOpacity, transition: '1300ms' }}
+          style={{ opacity: PortifolioOpacity, transition: '1000ms' }}
         >
-          <img
-            src={profile}
+          <ProfileImg
+            src={logo}
             alt="profile"
-            style={{ width: '200px', height: '200px', borderRadius: '50%' }}
+            animate={{ rotate: 360 }}
+            transition={{ ease: 'linear', duration: 2, loop: Infinity }}
           />
-          <PortifolioText>
+          <PortifolioText
+            animate={{ opacity: [1, 0.2, 0.3, 0.4, 0.5, 1] }}
+            transition={{ ease: 'linear', duration: 2, loop: Infinity }}
+          >
             Olá, <br></br>Meu nome é Ricardo Lopes<br></br>
             Estou à procura de uma oportunidade como desenvolvedor junior.
             <br></br>
@@ -72,9 +79,13 @@ const Welcome: React.FC = () => {
           </PortifolioText>
         </PortifolioContainer>
         <WelcomeTitleContainer
-          style={{ marginLeft: WelcomeMarginLeftScale, transition: '1500ms' }}
+          style={{ marginLeft: WelcomeMarginLeftScale, transition: '1000ms' }}
         >
           <WelcomeTitle
+            style={{
+              transition: WelcomeTextTransitionOpacity,
+              opacity: WelcomeTextComeOpacity
+            }}
             transition={{ ease: 'easeOut', duration: 2.3 }}
             initial={{ y: -35 * 8.2, scale: 2 }}
             animate={{ y: 0, scale: [1, 1.5, 1] }}
